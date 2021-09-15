ac6316f62989d8ea73ce2efdf54ec943af0be374
ReAnimatedSFM <88766987+ReAnimatedSFM@users.noreply.github.com>
2021-09-15 13:04:08 +0200

    Add project files.

---
+++ EX20StreamReader.Opg.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX20StreamReader.Opg._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX20StreamReader
+{
+    class Program
+    {
+
+        private static List<int> GetNumbersFromFile(string url)
+        {
+            using (StreamReader reader = new StreamReader(url))
+            {
+                List<int> values = new List<int>();
+                while (!reader.EndOfStream)
+                {
+                    string line;
+                    line = reader.ReadLine();
+
+                    values.Add(int.Parse(line));
+
+                }
+                return values;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //int sum = 0;
+            //int counter = 0;
+
+            //using (StreamReader reader = new StreamReader(@"C:/Users/will5606/source/repos/EX20StreamReader/TextFile1.txt"))
+            //{
+            //    List<int> values = new List<int>();
+
+            //    while (!reader.EndOfStream)
+            //    {
+            //        /* OPG 1
+            //        string line;
+            //        line = reader.ReadLine();
+
+            //        counter += line.Count();
+            //        sum += int.Parse(line);
+            //        */
+
+            //        string line;
+            //        line = reader.ReadLine();
+
+            //        values.Add(int.Parse(line));
+            //    }
+
+            //    //Console.WriteLine("Summen af tallene i filen TextFile1.txt er " + sum + " og antallet af tal er " + counter);
+
+            //    Console.WriteLine("Summen af tallene i filen TextFile1.txt er -" + values.Sum() + "-, med et gennemsnit på -" + values.Average() + "-.");
+            //}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+            List<int> values = GetNumbersFromFile(@"C:\Users\will5606\source\repos\EX20StreamReader\TextFile1.txt");
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                Console.WriteLine(values[i]);
+            }
+
+            Console.ReadKey();
+
+            
+        }
+
+
+    }
+}
